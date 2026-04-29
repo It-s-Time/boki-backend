aba759f612d9a230f17b7e712f79140e06d6370d
Yongseon-Yoo <jade020823@naver.com>
2026-04-30 02:53:22 +0900

    init: spring boot project setup

---
+++ src/main/java/com/boki/backend/BokiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boki.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BokiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BokiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boki/backend/BokiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boki.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BokiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
