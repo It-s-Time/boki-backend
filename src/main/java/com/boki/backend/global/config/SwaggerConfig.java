5b355f3a41d3c99ed5e95c3f945d3220f16109a7
Yongseon-Yoo <jade020823@naver.com>
2026-05-01 06:09:15 +0900

    chore: Swagger 설정 추가

---
+++ src/main/java/com/boki/backend/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.boki.backend.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "JWT TOKEN";
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("BOKI API 명세서")
+                .description("BOKI 프로젝트 백엔드 API 명세서입니다.")
+                .version("0.0.1");
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList(SECURITY_SCHEME_NAME);
+
+        Components components = new Components()
+                .addSecuritySchemes(SECURITY_SCHEME_NAME, new SecurityScheme()
+                        .name(SECURITY_SCHEME_NAME)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+
+        return new OpenAPI()
+                .info(info)
+                .addServersItem(new Server().url("/"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
